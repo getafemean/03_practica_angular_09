b724560a9619d72e35da4eb09de596da8e5ae881
getafemean <getafemean@gmail.com>
2021-05-24 17:40:15 +0200

    resubida

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -9,7 +9,7 @@ import { PedidosService } from '../servicios/pedidos.service';
 export class InicioComponent implements OnInit {
 
   articulos: Array<any> = [
-    {id: '01', nombre: 'Nike Jordan', descripcion: 'Lorem ipsum'},
+    {id: '01', nombre: 'Nike Michael Jordan', descripcion: 'Lorem ipsum'},
     {id: '02', nombre: 'Adidas Nadal', descripcion: 'Lorem ipsum'},
     {id: '03', nombre: 'Puma Messi', descripcion: 'Lorem ipsum'}
   ]
